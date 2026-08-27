221d13368062b8a655327ef28829637b535da496
Chance <cranki11@vols.utk.edu>
2026-08-27 16:07:50 -0700

    more explicit error printouts

--- src/vid2avi/cli.py
+++ src/vid2avi/cli.py
@@ -1,11 +1,17 @@
 import argparse
 from pathlib import Path
 
-from ffmpeg import FFmpeg  # pyright: ignore[reportMissingTypeStubs]
+from ffmpeg import (  # pyright: ignore[reportMissingTypeStubs]
+    FFmpeg,
+    FFmpegFileNotFound,
+    FFmpegInvalidCommand,
+)
 
 
 def build_parser():
-    parser = argparse.ArgumentParser(description="Convert video files.")
+    parser = argparse.ArgumentParser(
+        description="Convert video file directories to avi."
+    )
 
     _ = parser.add_argument(
         "-d",
@@ -48,10 +54,18 @@ def main():
     ffmpeg = FFmpeg().option("y")
 
     for file in files:
-        ff = ffmpeg.input(file).output(
-            str(file.parent / file.stem) + ".avi", {"codec:v": "mjpeg"}
-        )
-        ff.execute()
+        try:
+            fp = str(file.parent / file.stem) + ".avi"
+            ff = ffmpeg.input(file).output(fp, {"codec:v": "mjpeg"})
+            _ = ff.execute()
+        except FFmpegFileNotFound as e:
+            print("An exception has been occurred!")
+            print("- Message from ffmpeg:", e.message)
+            print("- Arguments to execute ffmpeg:", e.arguments)
+        except FFmpegInvalidCommand as e:
+            print("An exception has been occurred!")
+            print("- Message from ffmpeg:", e.message)
+            print("- Arguments to execute ffmpeg:", e.arguments)
 
 
 if __name__ == "__main__":
